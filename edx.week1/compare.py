66da42ab697bb22868cb3ce697092dd6a5a0445e
Mario R <mario.tco@gmail.com>
2024-01-15 15:57:31 -0800

    Adding comparison test file.

---
+++ edx.week1/compare.py
@@ -0,0 +1,9 @@
+x = input("Enter value of X... ")
+y = input("Enter value of Y... ")
+
+if x < y:
+    print("X is less than Y.")
+elif y < x:
+    print("X is greater than Y.")
+else:
+    print("X and Y are equal.")
