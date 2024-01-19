cfd670f8cd259454886bb5ebca76284fc94fcddf
Mario R <mario.tco@gmail.com>
2024-01-18 20:01:44 -0800

    These two file are examples of how to create a module and import it into other files.

---
+++ edx.week4/sayings.py
@@ -0,0 +1,12 @@
+def main():
+    hello("world")
+    goodbye("dude")
+
+def hello(name):
+    print(f"hello, {name}")
+
+def goodbye(name):
+    print(f"bye, {name}")
+
+if __name__ == "__main__":
+    main()
